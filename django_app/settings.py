ef3082c43add55ad8549c4a643ec733eb2097d64
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-06 16:51:52 +0530

    ss

--- django_app/settings.py
+++ django_app/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split("
 # Application definition
 
 INSTALLED_APPS = [
+    'myapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
