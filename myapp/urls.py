a15fa56f263e4c568589737f67affc051a25f587
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-07 09:44:46 +0530

    s

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import home,espac
 
 urlpatterns = [
-    path("",home,name="home")
+    path("",home,name="home"),
+    path("espac",espac,name="espac"),
 
 ]
