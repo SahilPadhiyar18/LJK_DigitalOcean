374847260eea9aab59672b1ecea885f2be8e2468
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-07 09:44:18 +0530

    s

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class roomdata(models.Model):
+    rid = models.CharField(max_length=20)
+    espid = models.TextField(max_length=20)
+    ac1 = models.BooleanField(default=False)
+    ac2 = models.BooleanField(default=False)
