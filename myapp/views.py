1a11538a7333b4d08c6b6ed3e179e7d29210d8ca
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-07 10:00:58 +0530

    ss

--- myapp/views.py
+++ myapp/views.py
@@ -3,15 +3,13 @@ from django.http import HttpResponse
 from .models import roomdata
 from django.contrib.auth.models import User, auth
 from django.template import loader
-import pandas as pd
 # Create your views here.
 
 def espac(request):
     a = roomdata(rid='ljp206', espid = 'ljp206',ac1=True,ac2=True)
     a.save()
-    df = roomdata.objects.all().values()
-    print("df",df)
-
+#     df = roomdata.objects.all().values()
+#     print("df",df)
     return HttpResponse("ok")
 
 def home(request):
