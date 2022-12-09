0174e9a54005793c55a6db397897ac25dc37541b
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-09 18:50:36 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home,espac
+from .views import *
 
 urlpatterns = [
     path("",home,name="home"),
     path("espac",espac,name="espac"),
+    # path("delete",delete,name="delete"),
+    path("cheakbox",cheakbox,name="cheakbox"),
+    path("data",data,name="data"),
+
+
 
 ]
