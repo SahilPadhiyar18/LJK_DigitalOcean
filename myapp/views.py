4c44a4f71799067e66b4566660a923ce6e2c8d4b
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-09 18:52:01 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,22 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import roomdata
+from .models import *
 from django.contrib.auth.models import User, auth
 from django.template import loader
+import pandas as pd
+import json
+from datetime import datetime
+from django.utils import timezone
+
 # Create your views here.
 
 def espac(request):
-    a = roomdata(rid='ljp206', espid = 'ljp206',ac1=True,ac2=True)
-    a.save()
-#     df = roomdata.objects.all().values()
-#     print("df",df)
-    return HttpResponse("ok")
+    roomid = request.GET['rid']
+    if roomdata.objects.filter(rid=roomid).exists():
+        ac1c = request.GET['ac1']
+        ac2c = request.GET['ac2']
+        data = roomdata.objects.filter(rid = roomid).values('ac1','ac2') 
+        roomdata.objects.filter(rid = roomid).update(ping = datetime.now(tz=timezone.utc))
+        datalogs(rid=roomid,ac1cur=ac1c,ac2cur=ac2c).save()
+        return HttpResponse(data)
+    return (HttpResponse("Sas"))
 
+    
 def home(request):
-    mydata = roomdata.objects.all()
+    mydata = roomdata.objects.all().order_by('rid').values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mydata,
+        }
+    return HttpResponse(template.render(context, request)) 
+
+def data(request):
+    mydata = datalogs.objects.all().order_by('rid').values()
     template = loader.get_template('home.html')
     context = {
         'mymembers': mydata,
         }
     return HttpResponse(template.render(context, request)) 
+
+
+def cheakbox(request):
+    try:
+        data = json.load(request)
+        roomdata.objects.filter(rid = data.get('rid') ).update(**{data.get('name'):data.get('value')})
+        return HttpResponse("data")
+    except:
+        return HttpResponse("pass")
     
