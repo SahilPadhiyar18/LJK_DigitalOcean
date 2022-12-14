c10a0a6fb6d8779dc913a54a4cd2b4f8cfa93efa
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-14 17:29:45 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     # path("delete",delete,name="delete"),
     path("cheakbox",cheakbox,name="cheakbox"),
     path("data",data,name="data"),
-
+    path("chart",chart,name="chart"),
+    path('population-chart/',population_chart, name='population-chart'),
+    path('acdetails',acdetails, name='acdetails'),
+    path('acupdate',acupdate, name='acupdate'),
 
 
 ]
