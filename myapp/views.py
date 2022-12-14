12df73276a770c1e2fb82747f7f07b3dbe3e6ca1
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-14 17:28:35 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -21,12 +21,13 @@ def espac(request):
         ac2c = request.GET['ac2']
         roomdata.objects.filter(rid = roomid).update(ping = datetime.now(tz=timezone.utc))
         datalogs(rid=roomid,ac1cur=ac1c,ac2cur=ac2c).save()
-        return JsonResponse(list(roomdata.objects.filter(rid=roomid).values('ac1','ac2')), safe=False)
+        # return JsonResponse(list(roomdata.objects.filter(rid = roomid).values('ac1','ac2')), safe=False)
+        return JsonResponse(list(roomdata.objects.filter(rid=roomid).values()), safe=False)
     return (HttpResponse("Sas"))
 
     
 # def home(request):
-#     mydata = roomdata.objects.all().order_by('rid').values()
+#     mydata = ac.objects.all().order_by('id').values()
 #     template = loader.get_template('index.html')
 #     context = {
 #         'mymembers': mydata,
@@ -41,8 +42,9 @@ def home(request):
         }
     return HttpResponse(template.render(context, request)) 
 
+
 def data(request):
-    mydata = datalogs.objects.all().order_by('-id').values()
+    mydata = datalogs.objects.all().order_by('id').values()
     template = loader.get_template('home.html')
     context = {
         'mymembers': mydata,
@@ -50,13 +52,6 @@ def data(request):
     return HttpResponse(template.render(context, request)) 
 
 
-# def cheakbox(request):
-#     try:
-#         data = json.load(request)
-#         roomdata.objects.filter(rid = data.get('rid') ).update(**{data.get('name'):data.get('value')})
-#         return HttpResponse("data")
-#     except:
-#         return HttpResponse("pass")
 def cheakbox(request):
     try:
         data = json.load(request)
@@ -64,3 +59,53 @@ def cheakbox(request):
         return HttpResponse("data")
     except:
         return HttpResponse("pass")
+    
+def chart(request):
+    labels = []
+    data = []
+    queryset = datalogs.objects.all()
+    for person in queryset:
+        labels.append(person.rid)
+        data.append(person.ac2cur)
+
+    return render(request,'data.html',{
+        'labels':labels,
+        'data':data
+    }) 
+
+def population_chart(request):
+    labels = []
+    data = []
+    queryset = datalogs.objects.all()
+    for person in queryset:
+        print(person.time)
+        labels.append(person.time)
+        data.append(person.ac2cur)
+
+    return JsonResponse(data={
+        'labels': labels,
+        'data': data,
+    })
+
+
+def acdetails(request):
+    try:
+        esp32id = request.GET['espid']
+        # print("A")
+        if ac.objects.filter(espid=esp32id).exists():
+            data = 0 
+            for i in ac.objects.filter(espid=esp32id).values('no'):
+                data = data*10 + i['no']
+            return HttpResponse(data)
+        else:
+            return HttpResponse("Name Not Found")
+    except: 
+        return HttpResponse("error 404")
+
+def acupdate(request):
+    if ac.objects.filter(espid=request.GET['espid']).exists():
+        esp32id = request.GET['espid']
+        return JsonResponse(list(ac.objects.filter(espid=esp32id).values('no','value')), safe=False)
+    # return JsonResponse(list(ac.objects.all().values('','value')), safe=False)
+    # # else:    
+    # return (HttpResponse("Sas"))  
