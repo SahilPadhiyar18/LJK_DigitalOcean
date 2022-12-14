a27ae97d79d7438cec54fd551bebfef41cbfb4ec
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-14 18:43:20 +0530

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(roomdata)
 admin.site.register(datalogs)
-admin.site.register(roomac)
+admin.site.register(roomdata)
+admin.site.register(ac)
