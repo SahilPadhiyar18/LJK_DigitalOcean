0b07a2306aace0fb9bfbd699f350e764d8f62950
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-12-14 17:17:22 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -15,6 +15,16 @@ class roomdata(models.Model):
     ac2lock = models.BooleanField(default=False)
     ping = models.DateTimeField(auto_now_add=True)
 
+class ac(models.Model):
+    no = models.IntegerField(max_length=20)
+    rid = models.CharField(max_length=20)
+    espid = models.TextField(max_length=20)
+    panelid = models.TextField(max_length=20)
+    name = models.TextField(max_length=20)
+    value = models.BooleanField(default=False)
+    lock = models.BooleanField(default=False)
+    ping = models.DateTimeField(auto_now_add=True) 
+
 class datalogs(models.Model):
     rid = models.CharField(max_length=20)
     ac1cur = models.FloatField()
