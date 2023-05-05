d5548c14e0bf8ccb23f09474a1d78b1fabd7c144
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2023-05-05 14:13:24 +0530

    janvi

--- myapp/urls.py
+++ myapp/urls.py
@@ -21,6 +21,10 @@ urlpatterns = [
     path('c3', c3, name="c3"),
     path('c4', c4, name="c4"),
     path('c5', c5, name="c5"),
+    path('c6', c6, name="c6"),
+    path('c7', c7, name="c7"),
+    path('c8', c8, name="c8"),
+    path('c9', c9, name="c9"),
     path('register', register, name="register"),
 
 ]
