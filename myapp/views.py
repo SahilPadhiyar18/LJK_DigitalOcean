bbdcef57880c03fb7c32d563866b769ad7d82acc
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2023-05-25 16:52:06 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -305,9 +305,9 @@ def register(request):
         registeruserdata = RegisterUserData(first_name=first_name, last_name=last_name, email=email, phone=phone,
                                             branch=branch, institute=institute, gender=gender, subject=subject)
         registeruserdata.save()
-         data = {
+        data = {
             'name': first_name +" "+ last_name,
-       
+    
             'email': email,
            'subject' : subject,
         }
